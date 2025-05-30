96498e1eb769e8041034c5c67bded034edf0a904
Alexander Gribkov <gribkovalexander@gmail.com>
2025-05-31 00:29:05 +0300

    Remove pr6 directory

--- pr6/grtib-app/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useUser } from "../user-context";
-import { hasRole } from "@/lib/auth";
-
-export default function AdminPage() {
-  const user = useUser();
-  if (!user || !hasRole(user, ["admin"])) return <div>Нет доступа</div>;
-  return <div>Страница администратора (Admin)</div>;
-} 
--- pr6/grtib-app/app/user/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useUser } from "../user-context";
-import { hasRole } from "@/lib/auth";
-
-export default function UserPage() {
-  const user = useUser();
-  if (!user || !hasRole(user, ["user"])) return <div>Нет доступа</div>;
-  return <div>Страница пользователя (User)</div>;
-} 
